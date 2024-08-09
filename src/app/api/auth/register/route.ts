3e960d1c5f4b763087012f50e5803cc5df463a99
Rahul <chattychidiya@gmail.com>
2024-08-09 19:56:11 +0530

    Remove bcrypt

--- src/app/api/auth/register/route.ts
+++ src/app/api/auth/register/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import bcrypt from "bcrypt";
+import bcrypt from "bcryptjs";
 import { prismaClient } from "@/lib/db";
 
 export const POST = async (req: Request) => {
