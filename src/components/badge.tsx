40793154e7134bcf6d08bfee1a44a4249f6a543b
Rahul <chattychidiya@gmail.com>
2024-08-16 17:15:16 +0530

    Made badge component

--- src/components/badge.tsx
+++ src/components/badge.tsx
@@ -7,19 +7,39 @@ type Props = {
 	className?: string;
 	onClick?: () => void;
 	color?: string;
+	hoverColor?: string;
+	hidden?: "sm" | "md" | "lg" | "xl" | "2xl";
+};
+
+const getDynamicColorClasses = (color: string, hoverColor?: string) => {
+	if (hoverColor)
+		return `bg-${color}-100/30 text-${color}-600 border-${color}-200 hover:bg-${hoverColor}-300/30 hover:border-${hoverColor}-300 hover:text-${hoverColor}-700 cursor-pointer`;
+
+	return `bg-${color}-100/30 text-${color}-600 border-${color}-200`;
+};
+
+const getDynamicHiddenClasses = (hidden: string | undefined) => {
+	if (!hidden) return "";
+	return `hidden ${hidden}:inline-flex`;
 };
 
 export default function BadgeButton({
 	text,
-	className,
+	className = "",
 	onClick,
-	color,
+	color = "blue",
+	hoverColor,
+	hidden,
 }: Props) {
 	return (
 		<Badge
 			variant={"outline"}
 			className={cn(
-				`border text-nowrap rounded-sm cursor-default transition-colors tabular-nums font-normal ` +
+				"border text-nowrap rounded-sm cursor-default transition-colors tabular-nums font-normal " +
+					getDynamicHiddenClasses(hidden) +
+					" " +
+					getDynamicColorClasses(color, hoverColor) +
+					" " +
 					className
 			)}
 			onClick={onClick}
