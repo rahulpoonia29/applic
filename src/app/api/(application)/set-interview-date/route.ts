674447636ff8bb8edbac2a9f1b09d2e1bd6281cd
Rahul <chattychidiya@gmail.com>
2024-08-16 10:42:33 +0530

    update set interview date route

--- src/app/api/(application)/set-interview-date/route.ts
+++ src/app/api/(application)/set-interview-date/route.ts
@@ -1,25 +1,38 @@
 import { getSessionServer } from "@/auth";
 import { prismaClient } from "@/lib/db";
+import { isValid, parseISO } from "date-fns";
 import { NextResponse } from "next/server";
 
-// GET /api/set-interview-date?applicationId={date}&interviewDate={date}&sendEmail={boolean}
+// GET /api/set-interview-date?applicationId={id}&interviewDate={date}&sendEmail={boolean}
 // Set the interview date for an application
 export const GET = async (req: Request) => {
 	try {
 		const url = new URL(req.url);
 		const applicationId = url.searchParams.get("applicationId");
-		const interviewDate = Date.parse(
-			url.searchParams.get("interviewDate") as string
-		);
+		// interview date is in ISO format
+		const interviewDate = url.searchParams.get("interviewDate");
+		// Expected values are "0" or "1"
 		const sendEmail = url.searchParams.get("sendEmail");
+
+		// Check for required parameters and validate interviewDate as ISO string
 		if (
 			!applicationId ||
 			!interviewDate ||
 			!sendEmail ||
-			isNaN(Number(applicationId))
+			isNaN(Number(applicationId)) ||
+			!isValid(parseISO(interviewDate)) ||
+			!(sendEmail === "0" || sendEmail === "1")
 		) {
 			return NextResponse.json(
-				{ success: false, message: "Invalid parameters" },
+				{
+					success: false,
+					message: "Invalid parameters",
+					data: {
+						applicationId,
+						interviewDate,
+						sendEmail,
+					},
+				},
 				{ status: 400 }
 			);
 		}
@@ -35,6 +48,12 @@ export const GET = async (req: Request) => {
 			);
 		}
 
+		// Convert ISO string to Date object
+		const interviewDateObject = new Date(interviewDate);
+
+		// Convert sendEmail to boolean
+		const sendEmailBoolean = sendEmail === "1";
+
 		const application = await prismaClient.jobApplication.update({
 			where: {
 				id: parseInt(applicationId),
@@ -42,12 +61,8 @@ export const GET = async (req: Request) => {
 			},
 			data: {
 				interview: true,
-				interviewDate: new Date(interviewDate),
-				emailSentDate: new Date(
-					new Date(interviewDate).setDate(
-						new Date(interviewDate).getDate() - 1
-					)
-				),
+				interviewDate: interviewDateObject,
+				emailSentDate: sendEmailBoolean ? interviewDateObject : null,
 			},
 			select: {
 				id: true,
