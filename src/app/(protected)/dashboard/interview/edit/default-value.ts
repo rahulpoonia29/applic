91188e6f7fdf8dc4f49bb075753ba5100ca4022f
Rahul <chattychidiya@gmail.com>
2024-08-17 22:14:42 +0530

    ui changes

--- src/app/(protected)/dashboard/interview/edit/default-value.ts
+++ src/app/(protected)/dashboard/interview/edit/default-value.ts
@@ -1,95 +1,179 @@
 export const defaultValue = {
-  type: "doc",
-  content: [
-    {
-      type: "paragraph",
-      content: [
-        {
-          type: "text",
-          text: "This is an example for the editor",
-        },
-      ],
-    },
-    {
-      type: "heading",
-      attrs: {
-        level: 1,
-      },
-      content: [
-        {
-          type: "text",
-          text: "H1",
-        },
-      ],
-    },
-    {
-      type: "heading",
-      attrs: {
-        level: 2,
-      },
-      content: [
-        {
-          type: "text",
-          text: "H2",
-        },
-      ],
-    },
-    {
-      type: "heading",
-      attrs: {
-        level: 3,
-      },
-      content: [
-        {
-          type: "text",
-          text: "H3",
-        },
-      ],
-    },
-    {
-      type: "paragraph",
-      content: [
-        {
-          type: "text",
-          text: "text",
-        },
-      ],
-    },
-    {
-      type: "bulletList",
-      attrs: {
-        tight: true,
-      },
-      content: [
-        {
-          type: "listItem",
-          content: [
-            {
-              type: "paragraph",
-              content: [
-                {
-                  type: "text",
-                  text: "new idea",
-                },
-              ],
-            },
-          ],
-        },
-        {
-          type: "listItem",
-          content: [
-            {
-              type: "paragraph",
-              content: [
-                {
-                  type: "text",
-                  text: "idea",
-                },
-              ],
-            },
-          ],
-        },
-      ],
-    },
-  ],
+	type: "doc",
+	content: [
+		{
+			type: "heading",
+			attrs: {
+				level: 1,
+			},
+			content: [
+				{
+					type: "text",
+					text: "Interview Details",
+				},
+			],
+		},
+		{
+			type: "paragraph",
+			content: [
+				{
+					type: "text",
+					text: "Company: ",
+				},
+			],
+		},
+		{
+			type: "paragraph",
+			content: [
+				{
+					type: "text",
+					text: "Role: ",
+				},
+			],
+		},
+		{
+			type: "paragraph",
+			content: [
+				{
+					type: "text",
+					text: "Date & Time: ",
+				},
+			],
+		},
+		{
+			type: "paragraph",
+			content: [
+				{
+					type: "text",
+					text: "Interviewer(s): ",
+				},
+			],
+		},
+		{
+			type: "paragraph",
+			content: [
+				{
+					type: "text",
+					text: "Interview Format: (e.g., Virtual, In-Person) ",
+				},
+			],
+		},
+		{
+			type: "paragraph",
+			content: [
+				{
+					type: "text",
+					text: "Platform/Location: (e.g., Zoom, Office Address) ",
+				},
+			],
+		},
+		{
+			type: "heading",
+			attrs: {
+				level: 2,
+			},
+			content: [
+				{
+					type: "text",
+					text: "Preparation Notes",
+				},
+			],
+		},
+		{
+			type: "paragraph",
+			content: [
+				{
+					type: "text",
+					text: "1. ",
+				},
+			],
+		},
+		{
+			type: "paragraph",
+			content: [
+				{
+					type: "text",
+					text: "2. ",
+				},
+			],
+		},
+		{
+			type: "heading",
+			attrs: {
+				level: 2,
+			},
+			content: [
+				{
+					type: "text",
+					text: "Follow-Up Plan",
+				},
+			],
+		},
+		{
+			type: "paragraph",
+			content: [
+				{
+					type: "text",
+					text: "1. Send thank-you email on ",
+				},
+			],
+		},
+		{
+			type: "paragraph",
+			content: [
+				{
+					type: "text",
+					text: "2. Follow up on ",
+				},
+			],
+		},
+		{
+			type: "heading",
+			attrs: {
+				level: 2,
+			},
+			content: [
+				{
+					type: "text",
+					text: "Interview Outcomes",
+				},
+			],
+		},
+		{
+			type: "bulletList",
+			attrs: {
+				tight: true,
+			},
+			content: [
+				{
+					type: "listItem",
+					content: [
+						{
+							type: "paragraph",
+							content: [
+								{
+									type: "text",
+									text: "Feedback Received: ",
+								},
+							],
+						},
+					],
+				},
+				{
+					type: "listItem",
+					content: [
+						{
+							type: "paragraph",
+							content: [
+								{
+									type: "text",
+									text: "Next Steps: ",
+								},
+							],
+						},
+					],
+				},
+			],
+		},
+	],
 };
--- src/app/(protected)/dashboard/interview/edit/page.tsx
+++ src/app/(protected)/dashboard/interview/edit/page.tsx
@@ -1,6 +1,5 @@
 "use client";
 import Editor from "@/components/editor/advanced-editor";
-// import { ThemeToggle } from "@/components/theme-toggle";
 import { JSONContent } from "novel";
 import { useState } from "react";
 import { defaultValue } from "./default-value";
@@ -12,7 +11,9 @@ export default function Home() {
 		<main className="flex min-h-screen w-full flex-col items-center justify-between  px-3 py-4 xl:px-10 xl:py-8">
 			<div className="flex w-full flex-col gap-6 rounded-md border bg-card p-6">
 				<div className="flex justify-between">
-					<h1 className="text-4xl font-semibold"> Novel Example</h1>
+					<h1 className="text-4xl font-semibold">
+						Add info about interview
+					</h1>
 				</div>
 				<Editor initialValue={value} onChange={setValue} />
 			</div>
