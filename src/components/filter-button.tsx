bfcd2f1f600d2286395c751476db3c2ed00d1f73
Rahul <chattychidiya@gmail.com>
2024-09-05 20:24:10 +0530

    Implement filter button and update search box

--- src/components/filter-button.tsx
+++ src/components/filter-button.tsx
@@ -9,47 +9,135 @@ import {
 	PopoverTrigger,
 } from "@/components/ui/popover";
 import { Separator } from "@/components/ui/separator";
+import useQueryParams from "@/lib/useQueryParams";
 import { ChevronDown } from "lucide-react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+
+interface FilterOptions {
+	status: string[];
+	jobType: string[];
+}
+
+interface FilterValues {
+	status: Record<string, boolean>;
+	jobType: Record<string, boolean>;
+}
+
+const filterOptions: FilterOptions = {
+	status: ["Saved", "Applied", "Interview", "Offered"],
+	jobType: ["Onsite", "Remote", "Hybrid"],
+};
+
+const createFilterValues = (options: string[], selectedOptions: string[]) =>
+	Object.fromEntries(
+		options.map((option) => [
+			option,
+			selectedOptions.includes(option.toLowerCase()),
+		]),
+	);
 
 export function FilterButton() {
-	const [isOpen, setIsOpen] = useState(false);
+	const { query, setMultipleQueries, clearAllQueries } = useQueryParams({
+		status: "",
+		jobType: "",
+	});
 
-	const filterOptions = {
-		status: [
-			"Saved",
-			"Applied",
-			"Interview",
-			"Offered",
-			// "Declined",
-		],
-		jobType: ["Onsite", "Remote", "Hybrid"],
+	const [filterValues, setFilterValues] = useState<FilterValues>({
+		status: createFilterValues(
+			filterOptions.status,
+			query.status?.split("+") || [],
+		),
+		jobType: createFilterValues(
+			filterOptions.jobType,
+			query.jobType?.split("+") || [],
+		),
+	});
+
+	useEffect(() => {
+		setFilterValues({
+			status: createFilterValues(
+				filterOptions.status,
+				query.status?.split("+") || [],
+			),
+			jobType: createFilterValues(
+				filterOptions.jobType,
+				query.jobType?.split("+") || [],
+			),
+		});
+	}, [query]);
+
+	const handleFilterChange = (
+		category: keyof FilterValues,
+		option: string,
+		checked: boolean,
+	) => {
+		setFilterValues((prev) => ({
+			...prev,
+			[category]: {
+				...prev[category],
+				[option]: checked,
+			},
+		}));
+	};
+
+	const applyFilters = () => {
+		setMultipleQueries({
+			status: Object.keys(filterValues.status)
+				.filter((key) => filterValues.status[key])
+				.map((key) => key.toLowerCase())
+				.join("+"),
+			jobType: Object.keys(filterValues.jobType)
+				.filter((key) => filterValues.jobType[key])
+				.map((key) => key.toLowerCase())
+				.join("+"),
+		});
 	};
 
 	return (
-		<Popover open={isOpen} onOpenChange={setIsOpen}>
+		<Popover>
 			<PopoverTrigger asChild>
 				<Button
 					variant="outline"
 					className="flex h-fit gap-2 px-3 py-2"
 				>
-					{/* <Filter className="h-4 w-4" /> */}
 					Filters
 					<ChevronDown className="h-4 w-4" />
 				</Button>
 			</PopoverTrigger>
 			<PopoverContent className="mr-6 p-1" align="start">
-				<div className="grid grid-cols-2 gap-3 p-4">
+				<div className="grid grid-cols-2 gap-3 px-4 py-2">
 					{Object.entries(filterOptions).map(
-						([category, options]) => (
-							<div key={category}>
+						([category, options]: [string, string[]]) => (
+							<div key={category} className="space-y-2">
+								<Label
+									htmlFor={category}
+									className="text-sm font-semibold capitalize"
+								>
+									{category}
+								</Label>
 								<div className="space-y-2">
-									{options.map((option) => (
+									{options.map((option: string) => (
 										<div
 											key={option}
-											className="flex items-center justify-start space-x-2"
+											className="flex items-center space-x-2"
 										>
-											<Checkbox id={option} />
+											<Checkbox
+												id={option}
+												checked={
+													filterValues[
+														category as keyof FilterValues
+													][option] || false
+												}
+												onCheckedChange={(
+													checked: boolean,
+												) =>
+													handleFilterChange(
+														category as keyof FilterValues,
+														option,
+														checked,
+													)
+												}
+											/>
 											<Label
 												htmlFor={option}
 												className="text-nowrap text-sm"
@@ -67,13 +155,13 @@ export function FilterButton() {
 				<div className="flex items-center justify-between bg-gray-50 px-2 py-2">
 					<Button
 						variant="outline"
-						onClick={() => setIsOpen(false)}
+						onClick={clearAllQueries}
 						className="flex h-fit gap-2 px-3 py-2"
 					>
-						Cancel
+						Clear all
 					</Button>
 					<Button
-						onClick={() => setIsOpen(false)}
+						onClick={applyFilters}
 						className="flex h-fit gap-2 px-3 py-2"
 					>
 						Apply Filters
--- src/components/sidemenu/searchInput.tsx
+++ src/components/sidemenu/searchInput.tsx
@@ -1,30 +1,18 @@
 "use client";
 
-import { useEffect, useState } from "react";
-import { Input } from "../ui/input";
-import { Search } from "lucide-react";
+import useQueryParams from "@/lib/useQueryParams";
 import { cn } from "@/lib/utils";
-import { usePathname, useRouter } from "next/navigation";
-import { useTimeout } from "usehooks-ts";
+import { Search } from "lucide-react";
+import { Input } from "../ui/input";
 
 type Props = {
 	className?: string;
 };
 
 function SearchInput({ className }: Props) {
-	const [searchValue, setSearchValue] = useState<string>("");
-	const router = useRouter();
-	const pathName = usePathname();
-
-	useEffect(() => {
-		const params = new URLSearchParams(window.location.search);
-		if (searchValue) {
-			params.set("search", searchValue);
-		} else {
-			params.delete("search"); // Remove the param if search is empty
-		}
-		router.push(`?${params.toString()}`);
-	}, [searchValue, pathName, router]);
+	const { query, setQuery } = useQueryParams({
+		search: "",
+	});
 
 	return (
 		<div
@@ -37,8 +25,8 @@ function SearchInput({ className }: Props) {
 			<Input
 				placeholder="Search"
 				className="h-fit border-none bg-transparent p-0 text-sm text-gray-700 placeholder:text-muted-foreground focus:ring-0 focus-visible:ring-0 focus-visible:ring-offset-0"
-				value={searchValue}
-				onChange={(e) => setSearchValue(e.target.value)}
+				value={query.search}
+				onChange={(e) => setQuery("search", e.target.value)}
 			/>
 		</div>
 	);
